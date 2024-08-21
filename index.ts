6b446d5def420336e8678078e325294fd8f2d7ad
Nitika Thingalaya <110655416+NitikaThingalaya-pki@users.noreply.github.com>
2024-08-21 18:11:48 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,12 +1,17 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResultV2, Handler } from 'aws-lambda';
 import * as _ from 'lodash';
+import * as qs from 'qs';
 
 export const handler: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> => {
   const max = 999;
   const val = _.random(max);
+
+  // Parse the query string from the event
+  const parsedQuery = qs.parse(event.queryStringParameters);
+
   const response = {
     statusCode: 200,
-    body: `The random value (max ${max}) is: ${val}`,
+    body: `The random value (max ${max}) is: ${val}, Parsed Query: ${JSON.stringify(parsedQuery)}`,
   };
   return response;
 };
